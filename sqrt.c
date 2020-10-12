c942068beda4c372d22d9c9f7ced896c750c171a
HarjotSingh-b18055 <harjoting7@gmail.com>
2020-10-12 20:18:14 +0530

    Added support for negative numbers

--- sqrt.c
+++ sqrt.c
@@ -15,11 +15,14 @@ int main(int argc, char* argv[]) {
 	int input = atoi(argv[1]);
 	
 	if(input<0){
-		printf("Invalid input! Negative number. Exiting...\n");
-		exit(-1);
+		printf("Sqrt of %d is %fi\n",input,sqrt(-input));
+		
+	}
+	else{
+		printf("Sqrt of %d is %f\n",input,sqrt(input));
 	}
 	
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	
 	printf("End of program. Exiting ...\n");
 	return(0);
 
